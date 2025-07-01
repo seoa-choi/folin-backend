d9c9c49156ebd6d1b150952126e87d952f2cfb71
seoa-choi <[choichoi120@naver.com](mailto:dalhoya@naver.com)>
2025-07-01 17:32:36 +0900

    feat: 시리즈Id db 수정, proposal 기준으로 테이블 합침

--- routes/series.js
+++ routes/series.js
@@ -64,21 +64,51 @@ router.get('/series/main', (req, res) => {
   });
 });
 
+// router.get('/series/:seriesId', (req, res) => {
+//   const pDetail = `SELECT
+//     ct.title_id,
+//     ct.title AS series_title,
+//     p.proposal_id,
+//     p.why,
+//     p.for_whom1,
+//     p.for_whom2,
+//     p.for_whom3,
+//     p.created_at
+//     FROM proposal p
+//     INNER JOIN contents_title ct ON p.title_id = ct.title_id
+//     ORDER BY ct.title_id, p.proposal_id;`;
+
+//   connection.query(pDetail, (err, result) => {
+//     if (err) {
+//       console.error('error', err.message);
+//       return res.status(500).send('Database error');
+//     }
+//     res.json(result);
+//   });
+// });
+
 router.get('/series/:seriesId', (req, res) => {
-  const pDetail = `SELECT
-    ct.title_id,
-    ct.title AS series_title,
+  const query = `SELECT
+    contents_id,
+    p.title_id,
     p.proposal_id,
+    ct.title,
+    sc.sub_title,
+    sc.linkers,
+    sc.img_url,
+    sc.content_type,
     p.why,
     p.for_whom1,
     p.for_whom2,
     p.for_whom3,
+    sc.created_at,
     p.created_at
-    FROM proposal p
-    INNER JOIN contents_title ct ON p.title_id = ct.title_id
-    ORDER BY ct.title_id, p.proposal_id;`;
+    FROM
+    proposal p
+    LEFT JOIN series_contents sc ON p.title_id = sc.title_id
+    LEFT JOIN contents_title ct ON p.title_id = ct.title_id;`;
 
-  connection.query(pDetail, (err, result) => {
+  connection.query(query, (err, result) => {
     if (err) {
       console.error('error', err.message);
       return res.status(500).send('Database error');
@@ -167,55 +197,3 @@ router.get('/series', (req, res) => {
 });
 
 module.exports = router;
-
-//   connection.query(seriesTab, (err, seriesResult) => {
-//     if (err) {
-//       console.error('에러:', err.message);
-//       return res.status(500).send('Database error');
-//     }
-
-//     connection.query(gridSeries, [limit, offset], (err, gridResult) => {
-//       if (err) {
-//         console.error('에러:', err.message);
-//         return res.status(500).send('Database error');
-//       }
-
-//       // 시리즈 타이틀 기준으로 묶기(슬라이드)
-//       const grouped = {};
-//       gridResult.forEach((item) => {
-//         const title = item.series_title;
-//         if (!grouped[title]) {
-//           grouped[title] = [];
-//         }
-//         grouped[title].push({
-//           contents_id: item.contents_id,
-//           sub_title: item.sub_title,
-//           linkers: item.linkers,
-//           img_url: item.img_url,
-//           content_type: item.content_type,
-//           created_at: item.created_at,
-//         });
-//       });
-
-//       const groupedSeries = Object.entries(grouped).map(([title, items]) => ({
-//         series_title: title,
-//         items: items.map((item) => ({
-//           ...item,
-//           series_title: title,
-//         })),
-//       }));
-
-//       const paginatedSeries = groupedSeries.slice(offset, offset + limit);
-
-//       res.json({
-//         seriesTab: seriesResult,
-//         gridSeries: paginatedSeries,
-//         page,
-//         limit,
-//         totalCount: groupedSeries.length,
-//       });
-//     });
-//   });
-// });
-
-// module.exports = router;
